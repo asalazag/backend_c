aecde958087ed5ce51c27e5e021f32a64fc5f47e
asalazag <116469356+asalazag@users.noreply.github.com>
2023-12-04 21:29:14 -0500

    Delete BackendApp/views/api directory

--- BackendApp/views/api/__init__.py
+++
@@ -1,5 +0,0 @@
-from .api import *
-from .active_integrations import *
-from .shipstation import *
-from .dynamic_queries import *
-from .dynamic_queries_execute import *
--- BackendApp/views/api/shipstation/__init__.py
+++
@@ -1,1 +0,0 @@
-from .create_label import *

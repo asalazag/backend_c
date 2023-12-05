f5b0df8270b76eb4cca5f0ab371e91905f648b33
asalazag <116469356+asalazag@users.noreply.github.com>
2023-12-04 21:24:46 -0500

    Delete app.py

--- app.py
+++
@@ -1,7 +0,0 @@
-
-from settings.models import *
-from django.conf import settings
-
-if __name__ == '__main__' : 
-    database_config = settings.DATABASES['sandbox']['NAME']
-    print(database_config)
